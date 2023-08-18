c68e18633c33553a58bdd5fe270354b14f9b93af
Esteban Russo <oerusso28@gmail.com>
2023-08-18 19:23:25 -0300

    practicando cruds en laravel

---
+++ database/migrations/2023_08_18_221609_create_clientes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('matricula',10);
+            $table->string('nombre',120);
+            $table->date('fecha_nacimiento');
+            $table->string('telefono',20);
+            $table->string('email',120);
+            $table->string('matricula',50)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clientes');
+    }
+};
